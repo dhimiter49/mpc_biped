b9b1426d267c5c5ce024462d25201ee99a381611
Dhimiter Pikuli <pikulidhimiter49@gmail.com>
2023-11-17 12:45:18 +0100

    added iterative dynamics model

--- mpc_biped.py
+++ mpc_biped.py
@@ -19,3 +19,12 @@ T = 9  # in ms
 period = 1 // dt  # in ms
 short_period = 0.8 // dt  # in ms
 diff_period = (period - short_period) // 2
+
+
+# iteration dynamics
+x_0 = np.zeros(3)
+dyn_mat = np.array([[1, dt, dt ** 2 / 2], [0, 1, dt], [0, 0, 1]])
+dyn_jerk = np.array([[1, dt, dt ** 2 / 2], [0, 1, dt], [0, 0, 1]])
+z_comp = np.array([1, 0, h_com / g])
+next_x = lambda x, x_jerk : dyn_mat @ x + x_jerk * dyn_jerk
+compute_z = lambda x : np.sum(z_comp * x)

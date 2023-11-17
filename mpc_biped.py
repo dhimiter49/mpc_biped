4beeb1b076defd28f361c0f22a4f3fe3b004e98b
Dhimiter Pikuli <pikulidhimiter49@gmail.com>
2023-11-17 12:44:53 +0100

    updated constants, use s not ms and period for harmony

--- mpc_biped.py
+++ mpc_biped.py
@@ -1,14 +1,21 @@
 import numpy as np
-import matplotlib
+import scipy
+import matplotlib.pyplot as plt
 
 # Constants
 max_constraint = 0.17
 min_constraint = -max_constraint
-max_min_constraint = -0.3
+max_min_constraint = -0.03
 min_max_constraint = -max_min_constraint
+start_time = 2.5  # in ms
+end_time = 7.5  # in ms
 g = 9.81  # gravity
 h_com = 0.8  # height of CoM
 R = 1
 Q = 1e6
-dt = 5
-N = 300  # lookahead
+dt = 0.005  # in ms
+N = 5  # lookahead
+T = 9  # in ms
+period = 1 // dt  # in ms
+short_period = 0.8 // dt  # in ms
+diff_period = (period - short_period) // 2

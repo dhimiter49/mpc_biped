correct z calculation

mpc_biped.py
@@ -25,7 +25,7 @@ diff_period = (period - short_period) // 2
 x_0 = np.zeros(3)
 dyn_mat = np.array([[1, dt, dt ** 2 / 2], [0, 1, dt], [0, 0, 1]])
 dyn_jerk = np.array([[dt ** 3 /6, dt ** 2 / 2, dt]])
-z_comp = np.array([1, 0, h_com / g])
+z_comp = np.array([1, 0, -h_com / g])
 next_x = lambda x, x_jerk : (dyn_mat @ x + x_jerk * dyn_jerk).flatten()
 compute_z = lambda x : np.sum(z_comp * x)
 
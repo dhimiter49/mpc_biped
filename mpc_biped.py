ba2691505d8494576cc4b1a67148e3cb14edff2e
Dhimiter Pikuli <pikulidhimiter49@gmail.com>
2023-11-30 15:37:28 +0100

    added intial qp implemtnation

--- mpc_biped.py
+++ mpc_biped.py
@@ -25,7 +25,7 @@ short_period = 0.8 // dt  # in ms
 diff_period = (period - short_period) // 2
 
 
-# iteration dynamics
+# Iteration dynamics
 x_0 = np.zeros(3)
 dyn_mat = np.array([[1, dt, dt ** 2 / 2], [0, 1, dt], [0, 0, 1]])
 dyn_jerk = np.array([[dt ** 3 /6, dt ** 2 / 2, dt]])
@@ -33,9 +33,9 @@ z_comp = np.array([1, 0, -h_com / g])
 next_x = lambda x, x_jerk : (dyn_mat @ x + x_jerk * dyn_jerk).flatten()
 compute_z = lambda x : np.sum(z_comp * x)
 
-# constraints/bounds
-z_max = max_constraint * np.ones(int(T / dt))
-z_min = min_constraint * np.ones(int(T / dt))
+# Constraints/bounds
+z_max = max_constraint * np.ones(int(T / dt) + N)
+z_min = min_constraint * np.ones(int(T / dt) + N)
 z_max[int(start_time / dt) : int(start_time / dt + short_period)] = max_min_constraint
 z_max[
     int(start_time / dt + short_period + period) :
@@ -58,6 +58,15 @@ z_min[
     int(start_time / dt + 3 * short_period + 2 * period + diff_period) :
     int(start_time / dt + 4 * short_period + 2 * period + diff_period)
 ] = min_max_constraint
+
+P_x = np.ones((N, 3))
+P_x[:, 1] *= np.array([dt * i for i in range(1, N + 1)])
+P_x[:, 2] *= np.array([dt ** 2 * i ** 2 / 2 - h_com / g for i in range(1, N + 1)])
+
+P_u = scipy.linalg.toeplitz(
+    np.array([(1 + 3 * i + 3 * i ** 2) * dt ** 3 / 6 - dt * h_com / g for i in range(N)]),
+    np.zeros(N),
+)
 
 
 def calc_z_ref():
@@ -99,17 +108,6 @@ def calc_z_ref():
 
 def analytical_solution():
     z_ref = calc_z_ref()
-    P_x = np.ones((N, 3))
-    P_x[:, 1] *= np.array([dt * i for i in range(1, N + 1)])
-    P_x[:, 2] *= np.array([dt ** 2 * i ** 2 / 2 - h_com / g for i in range(1, N + 1)])
-
-    P_u = scipy.linalg.toeplitz(
-        np.array(
-            [(1 + 3 * i + 3 * i ** 2) * dt ** 3 / 6 - dt * h_com / g for i in range(N)]
-        ),
-        np.zeros(N),
-    )
-
     x_k = x_0
     z_cop = []
     x_com = []
@@ -128,8 +126,21 @@ def analytical_solution():
 
 def qp_solution():
     z_ref = calc_z_ref()[:int(T / dt)]
-    x_com = np.zeros(z_ref.shape)
-    z_cop = np.zeros(z_ref.shape)
+    x_k = x_0
+    z_cop = []
+    x_com = []
+    P = 10e6 * np.eye(N)
+    q = np.zeros(N)
+    for k in tqdm(range(int(T / dt))):
+        z_min_jerk = np.linalg.inv(P_u) @ (z_min[k : k + N] - P_x @ x_k)
+        z_max_jerk = np.linalg.inv(P_u) @ (z_max[k : k + N] - P_x @ x_k)
+        lb = np.minimum(z_min_jerk, z_max_jerk)
+        ub = np.maximum(z_min_jerk, z_max_jerk)
+        x_jerk = solve_qp(P, q, lb=lb, ub=ub, solver="osqp")
+        x_k = next_x(x_k, x_jerk[0])
+        # x_k += np.random.normal([0.0, 0.0, 0.0], [0.001, 0.0005, 0.0001])
+        x_com.append(x_k[0])
+        z_cop.append(compute_z(x_k))
     return z_ref, x_com, z_cop
 
 
@@ -139,8 +150,8 @@ else:
     z_ref, x_com, z_cop = analytical_solution()
 
 # plotting
-plt.plot(np.arange(0, T, dt), z_max, color='red', linestyle="dashed")
-plt.plot(np.arange(0, T, dt), z_min, color='blue', linestyle="dashed")
+plt.plot(np.arange(0, T, dt), z_max[:int(T / dt)], color='red', linestyle="dashed")
+plt.plot(np.arange(0, T, dt), z_min[:int(T / dt)], color='blue', linestyle="dashed")
 plt.plot(np.arange(0, T, dt), z_ref[:int(T / dt)], color='green', linestyle="dashed")
 plt.plot(np.arange(0, T, dt), np.array(x_com), color='black', linewidth=3)
 plt.plot(np.arange(0, T, dt), np.array(z_cop), color='black')
